a4c684eca2d31df05864a3477650ee443d98184b
Jedilson <jedilson2012@yahoo.com.br>
2014-11-29 00:17:50 -0300

    Reorganização do padrão da pasta ProjetoUnibratec

--- DatabasePocketQueue/DAO/Database/IRepositorio/IRepositorioEstadoSenha.cs
+++
@@ -1,9 +0,0 @@
-﻿using DatabasePocketQueue.DAO.Entidades;
-
-namespace DatabasePocketQueue.DAO.Database.IRepositorio
-{
-    public interface IRepositorioEstadoSenha
-    {
-        bool InserirEstadoSenha(EstadoSenha estado);
-    }
-}
