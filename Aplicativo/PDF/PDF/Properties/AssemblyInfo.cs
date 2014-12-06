b1ac0137b88f5871ecd4337d1698f2d9d38515eb
FernandaAmanda <nandinha.hardman@hotmail.com>
2014-12-06 16:18:08 -0200

    Modelo de aplicatico que gera arquivo

---
+++ Aplicativo/PDF/PDF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informações gerais sobre um assembly são controladas através do seguinte 
+// conjunto de atributos. Altere o valor destes atributos para modificar a informação
+// associada a um assembly.
+[assembly: AssemblyTitle("PDF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PDF")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly não visíveis 
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
+[assembly: Guid("b5a262c0-16ed-4507-8432-cd7f53380704")]
+
+// Informações de Versão para um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
+// utilizando o '*' como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
